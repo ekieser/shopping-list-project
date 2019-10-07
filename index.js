518b7ed8e09f1263987c9c3752845efc2498b5ac
Ethan Kieser <ethankieser@gmail.com>
2019-10-07 16:50:48 -0500

    finished js

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-loadEvents();
-function loadEvents(){
-  document.querySelector('form').addEventListener('submit',submit);
-}
-
-$(function addText() {
-    $('.js-shopping-list-form').sumbit(function(event) {
+function addToList() {
+    $('#js-shopping-list-form').submit(function(event) {
         event.preventDefault();
         const userText = $('#shopping-list-entry').val();
-        $('#shopping-list-entry').val("");
-    });
-},
+        $('#shopping-list-entry').val('');
+        $('.shopping-list').prepend(
+            `<li>
+                <span class="shopping-item">${userText}</span>
+                <div class="shopping-item-controls">
+                    <button class="shopping-item-toggle">
+                    <span class="button-label">check</span>
+                    </button>
+                    <button class="shopping-item-delete">
+                    <span class="button-label">delete</span>
+                    </button>
+                </div>
+            </li>`
+            );
+        });
+};
 
-$(function addToList() {
-    $("button").click(function(event) {
-      $('.shopping-list').prepend(
-        "<li>" + ${userText} + "</li>"
-      );
-    });
-    
-},
-
-$(function editList() {
+function editList() {
     $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
         $(this).closest('li').remove();
-      });
-    
+    });
     $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
         $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-      });
-}
+    });
+};
 
+function loadEvents() {
+    addToList();
+    editList();
+};
+$(loadEvents);
